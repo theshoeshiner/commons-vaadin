3cd8cf4b79a09a4034df294078722fd05ffdc220
theshoeshiner <dcwatson84@gmail.com>
2021-04-20 16:26:29 -0400

    added new data provider designed to filter on a single search string

---
+++ src/main/java/org/thshsh/vaadin/StringFilterDataProvider.java
@@ -0,0 +1,151 @@
+package org.thshsh.vaadin;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.google.common.base.Preconditions;
+import com.google.common.primitives.Ints;
+import com.vaadin.flow.data.provider.BackEndDataProvider;
+import com.vaadin.flow.data.provider.CallbackDataProvider;
+import com.vaadin.flow.data.provider.ConfigurableFilterDataProvider;
+import com.vaadin.flow.data.provider.DataProvider;
+import com.vaadin.flow.data.provider.DataProviderListener;
+import com.vaadin.flow.data.provider.Query;
+import com.vaadin.flow.data.provider.QuerySortOrder;
+import com.vaadin.flow.function.SerializableBiFunction;
+import com.vaadin.flow.function.SerializableFunction;
+import com.vaadin.flow.shared.Registration;
+
+@SuppressWarnings("serial")
+public class StringFilterDataProvider<T, ID extends Serializable> implements ConfigurableFilterDataProvider<T, String,String> , BackEndDataProvider<T, String>{
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(ExampleFilterDataProvider.class);	
+	
+    private StringFilterRepository<T,ID> repository;
+    private List<QuerySortOrder> defaultSort;
+    private ConfigurableFilterDataProvider<T, String, String> delegate;
+
+    public StringFilterDataProvider(StringFilterRepository<T,ID> r, List<QuerySortOrder> defaultSort) {
+ 
+        this.repository = r;
+        this.defaultSort = defaultSort;
+        delegate = buildDataProvider();
+        
+        findAllFunction = repository::findAll;
+        findFilteredFunction = repository::searchByStringFilter;
+        countAllFunction = repository::count;
+        countFilteredFunction = repository::countByStringFilter;
+        
+        
+    }
+    
+    Function<Pageable,Page<T>> findAllFunction;
+    BiFunction<String,Pageable,Page<T>> findFilteredFunction;
+    Supplier<Long> countAllFunction;
+    Function<String,Long> countFilteredFunction;
+
+    private ConfigurableFilterDataProvider<T, String, String> buildDataProvider() {
+		  CallbackDataProvider<T, String> dataProvider = DataProvider.fromFilteringCallbacks(
+		        q -> q.getFilter()
+		                .map(filter -> findFilteredFunction.apply(filter, ChunkRequest.of(q, defaultSort)).getContent())
+		                .orElseGet(() -> findAllFunction.apply(ChunkRequest.of(q, defaultSort)).getContent())
+		                .stream(),
+		        q -> Ints.checkedCast(q
+		                .getFilter()
+		                .map(document -> countFilteredFunction.apply(document))
+		                .orElseGet(countAllFunction)));
+        
+	
+		  
+        
+    	
+        return dataProvider.withConfigurableFilter((q, c) -> c);
+    }
+    
+
+    @Override
+    public void setFilter(String filter) {
+        delegate.setFilter(filter);
+    }
+
+    @Override
+    public boolean isInMemory() {
+        return delegate.isInMemory();
+    }
+
+    @Override
+    public int size(Query<T, String> query) {
+        return delegate.size(query);
+    }
+
+    @Override
+    public Stream<T> fetch(Query<T, String> query) {
+        return delegate.fetch(query);
+    }
+
+    @Override
+    public void refreshItem(T item) {
+        delegate.refreshItem(item);
+    }
+
+    @Override
+    public void refreshAll() {
+        delegate.refreshAll();
+    }
+
+    @Override
+    public Object getId(T item) {
+        return delegate.getId(item);
+    }
+
+    @Override
+    public Registration addDataProviderListener(DataProviderListener<T> listener) {
+        return delegate.addDataProviderListener(listener);
+    }
+
+    @Override
+    public <C> DataProvider<T, C> withConvertedFilter(SerializableFunction<C, String> filterConverter) {
+        return delegate.withConvertedFilter(filterConverter);
+    }
+
+    @Override
+    public <Q, C> ConfigurableFilterDataProvider<T, Q, C> withConfigurableFilter(SerializableBiFunction<Q, C, String> filterCombiner) {
+        return delegate.withConfigurableFilter(filterCombiner);
+    }
+
+    @Override
+    public ConfigurableFilterDataProvider<T, Void, String> withConfigurableFilter() {
+        return delegate.withConfigurableFilter();
+    }
+
+
+	public void setFindAllFunction(Function<Pageable, Page<T>> findAllFunction) {
+		this.findAllFunction = findAllFunction;
+	}
+
+	public void setFindFilteredFunction(BiFunction<String, Pageable, Page<T>> findFilteredFunction) {
+		this.findFilteredFunction = findFilteredFunction;
+	}
+
+	public void setCountAllFunction(Supplier<Long> countAllFunction) {
+		this.countAllFunction = countAllFunction;
+	}
+
+	public void setCountFilteredFunction(Function<String, Long> countFilteredFunction) {
+		this.countFilteredFunction = countFilteredFunction;
+	}
+
+	@Override
+	public void setSortOrders(List<QuerySortOrder> sortOrders) {
+		throw new IllegalStateException("Not implemented!");
+	}
+}
---
+++ src/main/java/org/thshsh/vaadin/StringFilterRepository.java
@@ -0,0 +1,19 @@
+package org.thshsh.vaadin;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.Repository;
+
+public interface StringFilterRepository<T, ID> extends Repository<T,ID>, CrudRepository<T, ID>, PagingAndSortingRepository<T, ID> {
+
+	public default Page<T> searchByStringFilter(String s,Pageable p) {
+		return null;
+	}
+	
+	public default Long countByStringFilter(String s) {
+		return null;
+	}
+	
+}
