bb956392ac8394b94d385d9c942d822e76782d52
daniel.watson <daniel.watson@bioclinica.com>
2021-08-20 14:29:17 -0400

    yesno util method

--- src/main/java/org/thshsh/vaadin/entity/ConfirmDialogs.java
+++ src/main/java/org/thshsh/vaadin/entity/ConfirmDialogs.java
@@ -2,6 +2,7 @@ package org.thshsh.vaadin.entity;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 
 //TODO allow apps to override this
@@ -23,6 +24,22 @@ public class ConfirmDialogs {
 	
 	public static ConfirmDialog messageDialog(String title,String message) {
 		ConfirmDialog cd = new ConfirmDialog(title,message,VaadinIcon.INFO_CIRCLE.create());
+		return cd;
+	}
+	
+	public static ConfirmDialog yesNoDialog(String message,Runnable r) {
+		return yesNoDialog(null, message, VaadinIcon.QUESTION.create(), r);
+	}
+	
+	public static ConfirmDialog yesNoDialog(String header,String message,Icon icon,Runnable r) {
+		ConfirmDialog cd = new ConfirmDialog(header,message,icon);
+		cd
+		.withYesButton()
+		.with(r)
+		.withVariants(ButtonVariant.LUMO_PRIMARY)
+		.withKey(Key.ENTER)
+		;
+		cd.withNoButton();
 		return cd;
 	}
 	
