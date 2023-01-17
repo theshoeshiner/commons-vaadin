e3665aebcaa31ddb6f77bcf699bef2caa4315db7
daniel.watson <daniel.watson@bioclinica.com>
2023-01-16 20:31:54 -0500

    details

---
+++ src/main/java/org/thshsh/vaadin/nested/DetailsVerticalLayout.java
@@ -0,0 +1,8 @@
+package org.thshsh.vaadin.nested;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+@SuppressWarnings("serial")
+public class DetailsVerticalLayout extends VerticalLayout {
+
+}
