dffe01c69c5c79396438f41dfd9114252bb8af1c
daniel.watson <daniel.watson@bioclinica.com>
2023-06-26 09:07:53 -0400

    tab test

--- src/test/java/org/thshsh/vaadin/tabs/AddonView.java
+++ src/test/java/org/thshsh/vaadin/tabs/AddonView.java
@@ -1,11 +1,16 @@
 package org.thshsh.vaadin.tabs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.thshsh.vaadin.tabsheet.BasicTab;
 import org.thshsh.vaadin.tabsheet.BasicTabSheet;
+import org.thshsh.vaadin.tabsheet.BasicTabSheet.Orientation;
 
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.tabs.Tabs.Orientation;
 import com.vaadin.flow.router.Route;
 
 /**
@@ -16,17 +21,34 @@ import com.vaadin.flow.router.Route;
 public class AddonView extends Div {
 	
 	
+    private static final Logger LOGGER = LoggerFactory.getLogger(AddonView.class);
+
 	
 
     public AddonView() {
-
-        BasicTabSheet hor = createSheet();
-        hor.setOrientation(Orientation.HORIZONTAL);
-        add(hor);
         
-        BasicTabSheet vert = createSheet();
-        vert.setOrientation(Orientation.VERTICAL);
-        add(vert);
+        {
+            BasicTabSheet hor = createSheet();
+            hor.setOrientation(Orientation.Horizontal);
+            add(hor);
+        }
+        
+        add(new Hr());
+        
+        {
+            BasicTabSheet vert = createSheet();
+            vert.setOrientation(Orientation.Vertical);
+            add(vert);
+        }
+        
+        add(new Hr());
+        
+        {
+            BasicTabSheet vert = createSheet();
+            vert.setDraggable(true);
+            vert.setOrientation(Orientation.Vertical);
+            add(vert);
+        }
         
         
     }
@@ -34,9 +56,24 @@ public class AddonView extends Div {
     public BasicTabSheet createSheet() {
         BasicTabSheet tabsheet = new BasicTabSheet();
         //add(tabsheet);
-        tabsheet.addTab("Tab1", new Span("my content1"));
-        tabsheet.addTab("Tab2",VaadinIcon.ANCHOR.create(), new Span("my content2"));
-        tabsheet.addTab("Tab3", new Span("my content3"));
+        
+        Icon icon = VaadinIcon.ALARM.create();
+        BasicTab dragTab = tabsheet.addTab("Tab1",icon, new Span("my content1"));
+        //DragSource<BasicTab> tabDragSource = DragSource.create(dragTab);
+        //tabDragSource.setDraggable(true);
+        icon = VaadinIcon.ANCHOR.create();
+        icon.addClassName(BasicTabSheet.getDragHandleClass());
+        tabsheet.addTab("Tab2",icon, new Span("my content2"));
+        
+        icon = VaadinIcon.MAILBOX.create();
+        icon.addClassName(BasicTabSheet.getDragHandleClass());
+        tabsheet.addTab("Tab3", icon, new Span("my content3"));
+        
+        icon = VaadinIcon.STOPWATCH.create();
+        icon.addClassName(BasicTabSheet.getDragHandleClass());
+        tabsheet.addTab("Tab4", icon ,new Span("my content 4"));
+        
+        tabsheet.setSelectedIndex(1);
         
         //tabsheet.getTabs().setOrientation(Orientation.VERTICAL);
         return tabsheet;
